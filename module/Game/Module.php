278dff99bfdc735f4645ebe4ddb252f768e2f2e8
Kaur Oks <kaur58@gmail.com>
2012-03-23 15:37:03 +0200

    start using zf2 beta 3. ajax and admin modules broken now :)

--- module/Game/Module.php
+++ module/Game/Module.php
@@ -8,9 +8,6 @@ use Zend\Module\Manager,
 
 class Module implements AutoloaderProvider
 {
-    protected $view;
-    protected $viewListener;
-
     public function init(Manager $moduleManager)
     {
         $events = StaticEventManager::getInstance();
@@ -38,46 +35,10 @@ class Module implements AutoloaderProvider
     
     public function initializeView($e)
     {
-        $app          = $e->getParam('application');
-        $locator      = $app->getLocator();
-        $config       = $e->getParam('config');
-        $view         = $this->getView($app);
-        $viewListener = $this->getViewListener($view, $config);
-        $app->events()->attachAggregate($viewListener);
-        $events       = StaticEventManager::getInstance();
-        $viewListener->registerStaticListeners($events, $locator);
-    }
-
-    protected function getViewListener($view, $config)
-    {
-        if ($this->viewListener instanceof View\Listener) {
-            return $this->viewListener;
-        }
-
-        $viewListener       = new View\Listener($view, $config->layout);
-        $viewListener->setDisplayExceptionsFlag($config->display_exceptions);
-
-        $this->viewListener = $viewListener;
-        return $viewListener;
-    }
-
-    protected function getView($app)
-    {
-        if ($this->view) {
-            return $this->view;
-        }
-
+        $app = $e->getParam('application');
+        $basePath = $app->getRequest()->getBasePath();
         $locator = $app->getLocator();
-        $view    = $locator->get('view');
-
-        // Set up view helpers        
-        $view->plugin('url')->setRouter($app->getRouter());
-        $view->doctype()->setDoctype('HTML5');
-
-        $basePath = $app->getRequest()->getBaseUrl();
-        $view->plugin('basePath')->setBasePath($basePath);
-
-        $this->view = $view;
-        return $view;
+        $renderer = $locator->get('Zend\View\Renderer\PhpRenderer');
+        $renderer->plugin('basePath')->setBasePath($basePath);
     }
 }
--- module/Game/src/Game/Controller/IndexController.php
+++ module/Game/src/Game/Controller/IndexController.php
@@ -2,13 +2,16 @@
 
 namespace Game\Controller;
 
-use Zend\Mvc\Controller\ActionController;
+use Zend\Mvc\Controller\ActionController,
+    Zend\View\Model\ViewModel;
 
 class IndexController extends ActionController
 {  
     public function indexAction()
     {
-        $this->getLocator()->get('view')->layout()->setLayout('homepage');
+        //$this->getLocator()->get('view')->layout()->setLayout('homepage');
         //return $this->redirect()->toRoute('foo-success');
+        //return new ViewModel();
+        return array();
     }
 }
